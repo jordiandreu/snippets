1dd58450d91ca3c1eb8722caa2150ad5ca204e8c
Jordi Andreu <jordiandreusegura@gmail.com>
2017-03-07 21:43:28 +0100

    Add argparse example from documentation.

--- argparse/main.py
+++ argparse/main.py
@@ -1,18 +1,18 @@
 import argparse
+#https://docs.python.org/2/howto/argparse.html
 
+parser = argparse.ArgumentParser(description="calculate X to the power of Y")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-v", "--verbose", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+args = parser.parse_args()
+answer = args.x**args.y
 
-def main():
-    parser = argparse.ArgumentParser(description='demo script')
-    parser.add_argument('-d', '--device', help='device name',
-                        required=True)
-    parser.add_argument('-e', '--encoding', help='encoding type',
-                        required=True)
-
-    args = parser.parse_args()
-
-    print 'Device name: %s' % args.device
-    print 'Encoding: %s' % args.encoding
-
-
-if __name__ == '__main__':
-    main()
+if args.quiet:
+    print answer
+elif args.verbose:
+    print "{} to the power {} equals {}".format(args.x, args.y, answer)
+else:
+    print "{}^{} == {}".format(args.x, args.y, answer)
