39ac57a11cbf9ed1de78b566e1c1ce6eacf09c61
Jordi Andreu <jordiandreusegura@gmail.com>
2019-10-09 21:55:11 +0200

    Fix singleton example

--- singleton/singleton_decorator.py
+++ singleton/singleton_decorator.py
@@ -1,27 +1,18 @@
-# Singleton/SingletonDecorator.py
 class SingletonDecorator:
     def __init__(self,klass):
         self.klass = klass
         self.instance = None
+
     def __call__(self,*args,**kwds):
-        if self.instance == None:
+        if self.instance is None:
             self.instance = self.klass(*args,**kwds)
         return self.instance
 
-# class foo: pass
-# foo = SingletonDecorator(foo)
+#@SingletonDecorator
+class Beamline(object):
+    def __init__(self):
+        print 'Initializing beamline instance of Beamline.'
 
-@SingletonDecorator
-class foo:
-    pass
 
-x=foo()
-y=foo()
-z=foo()
-x.val = 'sausage'
-y.val = 'eggs'
-z.val = 'spam'
-print(x.val)
-print(y.val)
-print(z.val)
-print(x is y is z)
+def get_beamline():
+    return Beamline()
